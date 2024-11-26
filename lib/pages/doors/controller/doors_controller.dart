364e3ce6916b4887b4603d4e4b6aa871242be929
dsarotti <sr.sarotti@gmail.com>
2024-11-26 20:59:32 +0100

    implemented logic for the autopilot mode

--- lib/pages/doors/controller/doors_controller.dart
+++ lib/pages/doors/controller/doors_controller.dart
@@ -1,5 +1,7 @@
-import 'package:get/get.dart';
+import 'dart:async';
 import 'dart:math';
+
+import 'package:get/get.dart';
 
 class DoorsController extends GetxController {
   var totalAttempts = 0.obs;
@@ -10,16 +12,18 @@ class DoorsController extends GetxController {
   var revealedDoor = 0.obs; // Door that gets revealed
   var isRevealed = false.obs; // Whether a door has been revealed or not
   var currentPrizeDoor = 0; // the door that contains the prize in the current try
-  var finishedAttempt=false.obs;
+  var finishedAttempt = false.obs;
 
+  //Autopilot related
+  var stopAutopilot = true;
+  var autopilotSwitchDoors = false; //Whether or not the autopilot will choose to switch doors after a goat door is revealed
 
   void startNewAttempt() {
-    finishedAttempt.value=false;
-    totalAttempts++;
+    finishedAttempt.value = false;
     userChoice.value = 0;
     revealedDoor.value = 0;
     isRevealed.value = false;
-    currentPrizeDoor = Random().nextInt(3)+1;
+    currentPrizeDoor = Random().nextInt(3) + 1;
   }
 
   void userSelectDoor(int doorSelected) {
@@ -31,9 +35,9 @@ class DoorsController extends GetxController {
     possibleDoors.remove(userChoice.value);
     possibleDoors.remove(currentPrizeDoor);
 
-    if(possibleDoors.length>1) {
+    if (possibleDoors.length > 1) {
       revealedDoor.value = possibleDoors[Random().nextInt(possibleDoors.length - 1)];
-    }else{
+    } else {
       revealedDoor.value = possibleDoors.first;
     }
     isRevealed.value = true;
@@ -49,16 +53,35 @@ class DoorsController extends GetxController {
     } else {
       incorrectAttempts++;
     }
-    finishedAttempt.value=true;
+    totalAttempts++;
+    finishedAttempt.value = true;
   }
 
-  double get successRate => totalAttempts.value > 0
-      ? (correctAttempts.value / totalAttempts.value)
-      : 0.0;
+  double get successRate => totalAttempts.value > 0 ? (correctAttempts.value / totalAttempts.value) : 0.0;
 
-  double get failureRate => totalAttempts.value > 0
-      ? (incorrectAttempts.value / totalAttempts.value)
-      : 0.0;
+  double get failureRate => totalAttempts.value > 0 ? (incorrectAttempts.value / totalAttempts.value) : 0.0;
+
+  Future<void> autopilot() async {
+    while (!stopAutopilot) {
+      //Start new attempt to clear the status
+      startNewAttempt();
+      await Future.delayed(Duration(seconds: 1));
+
+      userSelectDoor(Random().nextInt(3) + 1);
+      await Future.delayed(Duration(seconds: 1));
+
+      revealNonPrizeDoor();
+      await Future.delayed(Duration(seconds: 1));
+
+      if (autopilotSwitchDoors) {
+        userChangeChoice();
+        await Future.delayed(Duration(seconds: 1));
+      }
+
+      finalizeChoice();
+      await Future.delayed(Duration(seconds: 1));
+    }
+  }
 
   @override
   void onInit() {
